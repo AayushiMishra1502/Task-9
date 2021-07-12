f2a10cd9a621503d5119198cbafac10d089a66c6
AayushiMishra1502 <84334852+AayushiMishra1502@users.noreply.github.com>
2021-07-12 16:18:09 +0530

    Task9

---
+++ Task9/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+// fs.mkdirSync("Aayushi")
+
+// fs.writeFileSync("Aayushi/sample.txt", "Hello! My name is Aayushi Mishra.");
+
+// fs.appendFileSync('Aayushi/sample.txt','\nThis is a task for ACM SIG WEB.');
+
+// const data = fs.readFileSync('Aayushi/sample.txt','utf8');
+// console.log(data);
+
+// fs.renameSync('Aayushi/sample.txt','Aayushi/myBio.txt');
+
+// fs.unlinkSync("Aayushi/myBio.txt");
+
+fs.rmdirSync('Aayushi');
